996d1408911c121d1b209d6cb92073409ec773ea
Zankhna Gohil <zankhna@Jigneshs-MacBook-Pro.local>
2017-03-22 15:46:11 +0530

    Initial Commit

---
+++ MovieDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieDemo
+//
+//  Created by Zankhna Gohil on 22/03/17.
+//  Copyright © 2017 Zankhna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
